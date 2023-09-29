d7e2fbc9180aeb62981fc085558da88aea9ed714
ajpardos <105960369+ajpardos@users.noreply.github.com>
2023-09-29 16:23:04 -0500

    Update nodejsDockerDSL.groovy
    Update datos Julian

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js Docker DSL') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/ajpardos/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('ajpardos')
+            node / gitConfigEmail('julianpar@gmail.com')
         }
     }
     triggers {
@@ -14,9 +14,9 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('macloujulian/nodejsapp')
+            repositoryName('ajpardos/nodejsapp')
             tag('${GIT_REVISION,length=7}')
-            registryCredentials('docker-hub')
+            registryCredentials('ajpardo')
             forcePull(false)
             createFingerprints(false)
             skipDecorate()
